7350a5927f20571f198cbecb63542e55f793a834
Paulius Lazdauskas <paulius.lazdauskas1@gmail.com>
2023-05-10 23:12:46 +0300

    end

---
+++ lol.js
@@ -0,0 +1,5 @@
+const variableElementId = selection.id.split("data-level-")[1];
+
+const variableElementId2 = selection.id.split("data-level-")[1];
+
+const variableElementId3 = selection.id.split("data-level-")[1];
